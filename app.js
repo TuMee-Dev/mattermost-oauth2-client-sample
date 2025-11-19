2661d4757a2cddfda5fa6d30c7166835e95203a8
TuMee-Dev <bill@tumee.com>
2025-11-18 22:16:02 -0800

    Change session resave option from false to true

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(session({
   secret: 'qteyApp', 
-  resave: false, 
+  resave: true, 
   saveUninitialized: true,
   cookie: { 
     secure: true,
