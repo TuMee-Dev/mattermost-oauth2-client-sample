b3576bee280a31dc3b1a29b3cfd0765dffb5c3bb
TuMee-Dev <bill@tumee.com>
2025-11-18 22:27:12 -0800

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(session({
   secret: 'qteyApp', 
-  resave: true, 
+  resave: false, 
   saveUninitialized: true,
   cookie: { 
     secure: true,
